99ab8eb011a331e13042d0ef4b4856da0f35a563
Rene Vallecillo <reneval@gmail.com>
2013-11-18 15:54:50 -0600

    setting up rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reneval::Application.config.secret_key_base = '9bced993aa1491808c8af9292ad8fa47910e183d831c16d87754265d901e3c48144ea6a18ef8f4de33581f5628526223317fa4ef4b94cb981086abfda031bf7f'
