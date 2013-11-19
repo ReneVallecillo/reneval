b940ef7f66f5d61344a95936827a55755c068c5e
Rene Vallecillo <reneval@gmail.com>
2013-11-18 21:55:29 -0600

    setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Reneval::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  devise_for :users
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
