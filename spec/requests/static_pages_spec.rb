d6dc77a941e67161f9d0a4e72e54542e92d2fb8e
Rene Vallecillo <reneval@gmail.com>
2013-11-18 21:55:00 -0600

    tests for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,38 +1,27 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
   
   describe "Home page" do
-  	it "should have the content 'Sistema de Control de Proyectos'" do 
-  		visit '/static_pages/home'
-  		expect(page).to have_content('Sistema de Control de Proyectos')
-  	end
-  	it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("René Vallecillo & Consultores | Home")
-    end
+    before { visit root_path }
+
+  	it { should have_content('Sistema de Control de Proyectos') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-  	it "should have the content 'Ayuda'" do 
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Ayuda')
-  	end
-  	it "should have the title 'Ayuda'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("René Vallecillo & Consultores | Ayuda")
-    end
+    before { visit help_path }
+
+		it { should have_content('Ayuda') }
+    it { should have_title(full_title('Ayuda')) }
   end
 
   describe "About page" do
-  	it "should have the content 'Acerca de Nosotros'" do 
-  		visit '/static_pages/about'
-  		expect(page).to have_content('Acerca de Nosotros')
-  	end
+    before { visit about_path }
 
-  	it "should have the title 'Acerca de Nosotros'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("René Vallecillo & Consultores | Acerca de Nosotros")
-    end
+		it { should have_content('Acerca de Nosotros') }
+    it { should have_title(full_title('Acerca de Nosotros')) }
   end
 end
