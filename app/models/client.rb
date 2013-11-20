fffe9af09f4d77279f30acdb70073f9ab16263e4
Rene Vallecillo <reneval@gmail.com>
2013-11-20 11:04:42 -0600

    annotating models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: clients
+#
+#  id                  :integer          not null, primary key
+#  name                :string(255)
+#  fullname            :string(255)
+#  ruc                 :string(14)
+#  legalrepresentative :string(255)
+#  address             :string(255)
+#  web                 :string(255)
+#  created_at          :datetime
+#  updated_at          :datetime
+#
+
 class Client < ActiveRecord::Base
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: clients
+#
+#  id                  :integer          not null, primary key
+#  name                :string(255)
+#  fullname            :string(255)
+#  ruc                 :string(14)
+#  legalrepresentative :string(255)
+#  address             :string(255)
+#  web                 :string(255)
+#  created_at          :datetime
+#  updated_at          :datetime
+#
+
 require 'spec_helper'
 
 describe Client do
