c80a347c1d9ced9b8f5b71ca8556671a14beebb0
Rene Vallecillo <reneval@gmail.com>
2013-11-20 21:24:24 -0600

    first form for client creation

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,26 @@
 class ClientsController < ApplicationController
+  
   def show
   end
 
   def new
+    @client = Client.new
   end
+
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      flash[:success] = "Se ha creado el cliente #{@client.name}"
+      redirect_to @client
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def client_params
+      params.require(:client).permit(:name, :fullname,:ruc,
+        :legalrepresentative, :address, :web)
+    end
 end
--- spec/requests/client_pages_spec.rb
+++ spec/requests/client_pages_spec.rb
@@ -3,8 +3,35 @@ require 'spec_helper'
 describe "Client" do
   subject { page }
   describe "create new page" do
-    before { visit clients_new_path }
+    before { visit new_client_path }
     it { should have_content('Crear Cliente Nuevo') }
     it { should have_title(full_title('Crear Cliente')) }
   end
+
+  describe "register client" do
+    before { visit new_client_path }
+    let(:submit) { "Create Client" }
+
+    describe "with invalid information" do
+      it "should not create a client" do
+        expect { click_button submit }.to_not change(Client, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "client_name", with: "Example Name"
+        fill_in "client_fullname", with: "Example Fullname"
+        fill_in "client_ruc", with: "J1234567891112"
+        fill_in "client_legalrepresentative", with: "John Example"
+        fill_in "client_address", with: "Example Address"
+        fill_in "client_web", with: "www.example.com"
+      end
+
+      it "should create a new client" do
+        expect { click_button submit }.to change(Client, :count).by(1)
+      end
+    end
+  end
+
 end
