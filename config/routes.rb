df1989a7350f227c0c60fb35ebf957b13b73a462
Rene Vallecillo <reneval@gmail.com>
2013-11-25 19:31:01 -0600

    rails g dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Reneval::Application.routes.draw do
 
+  get "dashboard/index"
   devise_for :users
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
